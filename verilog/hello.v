85a24c1d03e27919ba5d5d7920d02448a231485f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-07-24 11:03:33 -0500

    Made the example more complicated.
    I decided that in order to really use Verilog for anything meaningful,
    you need to know more than just how to print a debug message, so I've
    added an example that shows basic module, registar, and test bench
    usage.

--- verilog/hello.v
+++ verilog/hello.v
@@ -1,8 +1,26 @@
-module main();
+module print_on_state_change(input in);
 
-initial
-  begin
+  always @ (posedge in) begin
+    $display("\"in\" changed from 0 to 1");
+  end
+
+  always @ (negedge in) begin
+    $display("\"in\" changed from 1 to 0");
+  end
+endmodule
+
+module test_bench();
+
+  reg in;
+  print_on_state_change instance_name(in);
+
+  initial begin
     $display("Hello, World!");
+
+    repeat(5) begin
+      in=0; #1;
+      in=1; #1;
+    end
     $finish;
   end
 endmodule
