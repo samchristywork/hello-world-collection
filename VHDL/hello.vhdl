ac302a9abb5ee32640c5679aebaf9af58a1743eb
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-07-27 22:50:18 -0500

    Extended the example to support two signals.
    This adds a signal called "comp", which mirrors clock, except that it is
    offset by one quarter period, and inverted.

--- VHDL/hello.vhdl
+++ VHDL/hello.vhdl
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity hello_world is
-  port (clk: inout std_logic);
+  port (clk: inout std_logic; comp: out std_logic);
 end hello_world;
 
 architecture behavior of hello_world
@@ -12,9 +12,15 @@ begin
   clk_process: process
   begin
     clk <= '0';
-    wait for period/2;
+    wait for period/4;
+
+    comp <= NOT clk;
+    wait for period/4;
 
     clk <= '1';
-    wait for period/2;
+    wait for period/4;
+
+    comp <= NOT clk;
+    wait for period/4;
   end process;
 end behavior;
