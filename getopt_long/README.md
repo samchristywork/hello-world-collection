0e7b930c3b96cd1f398c88d0cf5dfd8de6731165
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-04 22:02:47 -0500

    Added `getopt_long` "Hello, World!" example.
    This commit contains the source of the program, a Makefile to build it,
    and a README that lists dependencies.

---
+++ getopt_long/README.md
@@ -0,0 +1,11 @@
+## Overview
+
+`getopt_long` version of "Hello, World!". Build with `make` and run with
+`./build/hello`.
+
+## Dependencies
+
+```
+gcc
+make
+```
---
+++ getopt_long/hello.c
@@ -0,0 +1,52 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void usage(char *argv[]) {
+  fprintf(stderr,
+          "Usage: %s [-f foo] [-b bar] [file(s)]\n"
+          " -f,--foo   Some description goes here.\n"
+          " -b,--bar   Another description.\n"
+          " -h,--help      Print this usage message.\n"
+          " -v,--verbose   Display additional logging information.\n"
+          "",
+          argv[0]);
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+
+  int verbose = 0;
+
+  int opt;
+  int option_index = 0;
+  char *optstring = "hps:v";
+  static struct option long_options[] = {
+      {"help", no_argument, 0, 'h'},
+      {"print", no_argument, 0, 'p'},
+      {"something", required_argument, 0, 's'},
+      {"verbose", no_argument, 0, 'v'},
+      {0, 0, 0, 0},
+  };
+  while ((opt = getopt_long(argc, argv, optstring, long_options, &option_index)) != -1) {
+    if (opt == 'p') {
+      printf("Hello, World!\n");
+    } else if (opt == 'h') {
+      usage(argv);
+    } else if (opt == 'v') {
+      verbose = 1;
+    } else if (opt == '?') {
+      usage(argv);
+    } else {
+      puts(optarg);
+    }
+  }
+
+  if (optind < argc) {
+    int i = optind;
+    while (i < argc) {
+      fprintf(stdout, "Got additional argument: %s\n", argv[i]);
+      i++;
+    }
+  }
+}
