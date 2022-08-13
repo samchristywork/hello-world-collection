6b719fc42f379e8eab4f5b0e1256016f0941210d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-13 12:07:53 -0500

    Added comments.

--- ncurses/hello.c
+++ ncurses/hello.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+ * Handler for the resize signal.
+ */
 void signal_handler(int sig) {
   endwin();
   clear();
@@ -11,6 +14,9 @@ void signal_handler(int sig) {
 
 int main() {
 
+  /*
+   * Initialization.
+   */
   WINDOW *w;
   if ((w = initscr()) == NULL) {
     fprintf(stderr, "Error initializing ncurses.\n");
@@ -25,18 +31,30 @@ int main() {
   init_pair(1, COLOR_RED, COLOR_BLACK);
   init_pair(2, COLOR_GREEN, COLOR_BLACK);
 
+  /*
+   * Bind the signal handler to SIGWINCH. See signal(7).
+   */
   struct sigaction act;
   bzero(&act, sizeof(struct sigaction));
   act.sa_handler = signal_handler;
   sigaction(SIGWINCH, &act, NULL);
 
+  /*
+   * Main loop
+   */
   int c = 0;
   while (c != 'q') {
 
+    /*
+     * Print a string at the given coordinates.
+     */
     color_set(1, NULL);
     move(3, 3);
     printw("Hello, World!");
 
+    /*
+     * Print a formatted string.
+     */
     color_set(2, NULL);
     move(4, 3);
     printw("Key: %d  ", c);
@@ -68,6 +86,9 @@ int main() {
     c = getch();
   }
 
+  /*
+   * Cleanup.
+   */
   delwin(w);
   endwin();
   refresh();
