e03f6d529a7b9f39ab88f88685a2c84cab0222ea
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-18 17:27:25 -0500

    Added source files for glade program.

---
+++ graphics/glade/hello.c
@@ -0,0 +1,23 @@
+#include <gtk/gtk.h>
+
+void clickCallback(GtkButton *button, gpointer data) {
+  printf("Hello, World!\n");
+}
+
+int main(int argc, char *argv[]) {
+  gtk_init(&argc, &argv);
+
+  GtkBuilder *builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "hello.glade", NULL);
+
+  GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+  GtkWidget *button = GTK_WIDGET(gtk_builder_get_object(builder, "button"));
+
+  gtk_builder_connect_signals(builder, NULL);
+  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(clickCallback), NULL);
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  g_object_unref(builder);
+  gtk_widget_show(window);
+  gtk_main();
+}
