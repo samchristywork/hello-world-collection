1ae5d784ae5f6cdc43d8a4006e4a832ef771b134
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-29 17:39:54 -0600

    Added Test: Added (partial) test for sess. cookie
    Session cookies are kind of hard to test, but this has been very helpful
    for showing me how to test stateful web applications.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- libraries/actix-web/src/main.rs
+++ libraries/actix-web/src/main.rs
@@ -1,5 +1,6 @@
 use actix_files::Files;
-use actix_session::{storage::CookieSessionStore, SessionMiddleware};
+use actix_session::storage::CookieSessionStore;
+use actix_session::SessionMiddleware;
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
 use env_logger::Env;
@@ -14,7 +15,6 @@ static SESSION_SIGNING_KEY: &[u8] = &[0; 64]; // Just an example
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-
     let key = actix_web::cookie::Key::from(SESSION_SIGNING_KEY);
 
     let host = "0.0.0.0";
@@ -99,4 +99,78 @@ mod tests {
         let resp = test::call_and_read_body(&app, req).await;
         assert_eq!(resp, "{\"name\":\"foo\",\"name_double\":\"foofoo\"}");
     }
+
+    #[actix_web::test]
+    async fn test_get_session() {
+        env_logger::init_from_env(Env::default().default_filter_or("info"));
+        let key = actix_web::cookie::Key::from(SESSION_SIGNING_KEY);
+
+        let host = "0.0.0.0";
+        let port = 8080;
+        log::info!("Serving on {} port {}", host, port);
+        let mut app = test::init_service(
+            App::new()
+                .wrap(
+                    SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
+                        .cookie_secure(false)
+                        .build(),
+                )
+                .service(get_session::session),
+        )
+        .await;
+
+        /*
+         * Test base case
+         */
+        let req = test::TestRequest::get().uri("/session").to_request();
+        let result = test::call_and_read_body(&app, req).await;
+        assert_eq!(result, "You have visited 1 times");
+
+        /*
+         * Test with cookie
+         */
+        let req = test::TestRequest::get().uri("/session").to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        let cookie = resp.response().cookies().into_iter().next();
+
+        let req = test::TestRequest::get()
+            .uri("/session")
+            .cookie(cookie.unwrap())
+            .to_request();
+        let body = test::call_and_read_body(&app, req).await;
+        assert_eq!(body, "You have visited 2 times");
+
+        /*
+         * Test four subsequent calls
+         */
+        let req = test::TestRequest::get().uri("/session").to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        let cookie = resp.response().cookies().into_iter().next().unwrap();
+        let req = test::TestRequest::get()
+            .uri("/session")
+            .cookie(cookie)
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        let cookie = resp.response().cookies().into_iter().next().unwrap();
+        let req = test::TestRequest::get()
+            .uri("/session")
+            .cookie(cookie)
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        let cookie = resp.response().cookies().into_iter().next().unwrap();
+        let req = test::TestRequest::get()
+            .uri("/session")
+            .cookie(cookie)
+            .to_request();
+        let body = test::call_and_read_body(&app, req).await;
+
+        assert_eq!(body, "You have visited 4 times");
+    }
+
+    #[actix_web::test]
+    async fn test_main() {}
 }
