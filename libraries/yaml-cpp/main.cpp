5d9a538a3bc669ce8b9bb94ea5ee6831fbc05aea
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-03-11 19:27:33 -0600

    Feature: Implement traversal function

--- libraries/yaml-cpp/main.cpp
+++ libraries/yaml-cpp/main.cpp
@@ -4,6 +4,28 @@
 using namespace std;
 
 void traverseAndPrint(YAML::Node root, int depth = 0) {
+  if (root.IsMap()) {
+    for (YAML::iterator e = root.begin(); e != root.end(); ++e) {
+      for (int i = 0; i < depth; i++) {
+        cout << "  ";
+      }
+      cout << e->first.as<string>() << endl;
+      traverseAndPrint(e->second, depth + 1);
+    }
+  }
+
+  if (root.IsSequence()) {
+    for (YAML::iterator e = root.begin(); e != root.end(); ++e) {
+      traverseAndPrint(*e, depth + 1);
+    }
+  }
+
+  if (root.IsScalar()) {
+    for (int i = 0; i < depth; i++) {
+      cout << "  ";
+    }
+    cout << root.as<string>() << endl;
+  }
 }
 
 int main() {
