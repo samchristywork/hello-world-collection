8ae1b603422e811aafaef5524f323193b172b0dd
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-03-20 17:51:49 -0500

    Initial code release: Added sample code
    This code is meant to be a very bare-bones "Hello, World!" style
    project. The only real source of complexity here is that I wanted to
    have multiple compilation units to make sure my example worked for the
    most common use-case I have.

---
+++ tools/gcov/src/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo() {
+  printf("Hello, World!\n");
+  return 1;
+}
---
+++ tools/gcov/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "foo.h"
+
+void bar() {
+}
+
+int main() {
+  foo();
+  if(0){
+    printf("Hello, World!\n");
+  }else{
+    printf("Hello, World!\n");
+  }
+}
